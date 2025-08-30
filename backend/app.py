da6cd13eb90cbe55f82ee1109c4884a9a38b27aa
yourspetverify-stack <yourspetverify@gmail.com>
2025-08-30 15:38:57 +0530

    Replace all

--- backend/app.py
+++ backend/app.py
@@ -548,6 +548,10 @@ def get_user_id():
         return jsonify(success=True, user_id=user[0])
     return jsonify(success=False)
 
+@app.route('/')
+def home():
+    return "YoursPET API is running!"
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
